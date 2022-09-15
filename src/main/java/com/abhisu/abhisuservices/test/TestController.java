68c3fb8893a4e06f806711c16a7edabd94938473
Abhishek Raushan <abhik.raushan@gmail.com>
2022-09-15 14:21:41 +0530

    first restcontroller added

--- src/main/java/com/abhisu/abhisuservices/test/TestController.java
+++ src/main/java/com/abhisu/abhisuservices/test/TestController.java
@@ -1,7 +1,9 @@
 package com.abhisu.abhisuservices.test;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class TestController {
 
     @GetMapping("/test")
